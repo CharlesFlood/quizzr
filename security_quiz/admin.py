c6369bcc339a253b03946b446196e33305d7b7df
Charles Flood <charlesFlood3@gmail.com>
2018-04-07 20:57:46 -0700

    Changed name of quiz contents

--- security_quiz/admin.py
+++ security_quiz/admin.py
@@ -2,11 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Answer, Question, Quiz, QuizContents, Record
+from .models import Answer, Question, Quiz, QuizContent, Record
 # Register your models here.
 
 admin.site.register(Answer)
 admin.site.register(Question)
 admin.site.register(Quiz)
-admin.site.register(QuizContents)
+admin.site.register(QuizContent)
 admin.site.register(Record)
--- security_quiz/models.py
+++ security_quiz/models.py
@@ -7,18 +7,26 @@ from django.conf import settings
 class Answer(models.Model):
     text = models.CharField(max_length=100)
     category = models.CharField(max_length=12)
+    def __str__(self):
+        return self.text
 
 class Question(models.Model):
     text = models.CharField(max_length=250)
     answer = models.ForeignKey(Answer, on_delete=models.SET_NULL, null=True)
     category = models.CharField(max_length=12)
+    def __str__(self):
+        return self.text
 
 class Quiz(models.Model):
     name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
 
-class QuizContents(models.Model):
+class QuizContent(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.question.text
     
 class Record(models.Model):
     officer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
