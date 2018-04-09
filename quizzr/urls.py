9c395534b66e2da2d2e385b4cb0c21c3da12e197
Charles Flood <charlesFlood3@gmail.com>
2018-04-09 11:08:54 -0700

    Added elementary login/logout functionality

--- quizzr/urls.py
+++ quizzr/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^secquiz/', include('security_quiz.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+
 ]
